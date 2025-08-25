60c6e593366f16e500e5589ed75f885ab4519f08
Tobias Röhling <tobias.roehling@holisticon.de>
2025-08-25 15:22:52 +0200

    reproduce error with ng-zorro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+// import {NzDatePickerModule} from 'ng-zorro-antd/date-picker';
+import {CdkVirtualScrollViewport} from '@angular/cdk/scrolling';
+import {NzTableComponent} from 'ng-zorro-antd/table';
+
+// const nzDatepicker = NzDatePickerModule;
+const cdk = CdkVirtualScrollViewport;
+const nzTableComponent = NzTableComponent;
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet/*, CdkVirtualScrollViewport*/],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'component-id-collision';
+  // constructor() {
+  //   console.log(nzTableComponent, cdk);
+  // }
 }
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import {defineConfig} from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+import angular from '@analogjs/vite-plugin-angular';
+
+export default defineConfig(({mode}) => ({
+  plugins: [angular(), viteTsConfigPaths()],
+  test: {
+    globals: true,
+    setupFiles: ['src/test-setup.ts'],
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default']
+  },
+  define: {
+    'import.meta.vitest': mode !== 'production',
+  },
+}));
